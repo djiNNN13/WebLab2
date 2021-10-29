8c4c727c3f0b1c5039e9663e271d1bb4176e7987
djiNNN13 <48717495+djiNNN13@users.noreply.github.com>
2021-10-29 10:01:35 +0300

    Update JavaScript.js

--- JavaScript.js
+++ JavaScript.js
@@ -9,8 +9,15 @@ var res = (radius**2 * Math.PI);
 document.getElementById("task2").innerHTML += "The answer is: " + res;
 
 //task 3
-document.getElementById('calculate').onclick = () => getMinElement();
-if (document.cookie) hasCookies();
+if(document.cookie !== "" && confirm("Cookies: " + document.cookie + ". Want to save cookies?"))
+{
+    document.getElementById("getMinForm").setAttribute("class", "hidden");
+}
+else
+{
+    document.cookie = "MIN=0;max-age=0";
+}
+
 
 function getMinElement(){
     inputValue = document.getElementById("inputNum").value
@@ -23,23 +30,5 @@ function getMinElement(){
         inputValue = Math.trunc(inputValue/10)
     }
 alert(min)
-document.cookie = "Min=" + min.toString()
-}
-
-function hasCookies() {
-	if (confirm(document.cookie + "\n" + "Save?")) {
-		alert("Cookies are saved");
-		let form = document.forms.getMinForm
-		form.elements.numbers.style.visibility = 'hidden'
-		form.elements.calculate.style.visibility = 'hidden'
-	} else {
-		let cookies = document.cookie.split(";");
-		for (let i = 0; i < cookies.length; i++) {
-			let cookie = cookies[i]
-			let eqPos = cookie.indexOf("=")
-			let name = eqPos > -1 ? cookie.substr(0, eqPos) : cookie
-			document.cookie = name + "=;expires=Thu, 01 Jan 1970 00:00:00 GMT;"
-			document.cookie = name + '=; path=/; expires=Thu, 01 Jan 1970 00:00:01 GMT;'
-		}
-	}
+document.cookie = "MIN=" + min.toString()
 }
