a9f00317defb354a385b21b2dd789cb7557094b6
djiNNN13 <48717495+djiNNN13@users.noreply.github.com>
2021-11-12 10:39:31 +0300

    Update JavaScript.js

--- JavaScript.js
+++ JavaScript.js
@@ -16,19 +16,10 @@ if(document.cookie !== "" && confirm("Cookies: " + document.cookie + ". Want to 
 }
 else
 {
-    deleteAllCookies;
+    document.cookie.split(";")
+  .forEach(function(c) { 
+  	document.cookie = c.replace(/^ +/, "").replace(/=.*/, "=;expires=" + new Date().toUTCString() + ";path=/"); });
 }
-}
-
-function deleteAllCookies() {
-    var cookies = document.cookie.split(";");
-
-    for (var i = 0; i < cookies.length; i++) {
-        var cookie = cookies[i];
-        var eqPos = cookie.indexOf("=");
-        var name = eqPos > -1 ? cookie.substr(0, eqPos) : cookie;
-        document.cookie = name + "=;expires=Thu, 01 Jan 1970 00:00:00 GMT";
-    }
 }
 
 
